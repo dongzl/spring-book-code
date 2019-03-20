1744b501d0388f1603624b26f4dc6de777faf866
dongzl <dzllikelsw@163.com>
2019-03-20 16:05:52 +0800

    init spring-boot-step-by-step project

---
+++ spring-boot-step-by-step/src/main/java/com/spring/boot/step/StepByStepApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepByStepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StepByStepApplication.class, args);
+    }
+
+}
---
+++ spring-boot-step-by-step/src/test/java/com/spring/boot/step/StepByStepApplicationTests.java
@@ -0,0 +1,17 @@
+package com.spring.boot.step;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StepByStepApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
