0d1ffddb809d4669e0a1b359fb44b4a39205fed0
dongzl <dzllikelsw@163.com>
2019-06-06 17:02:58 +0800

    ratelimiter redis

---
+++ spring-boot-step-by-step/src/main/java/com/spring/boot/step/ratelimter/RateLimiter.java
@@ -0,0 +1,37 @@
+package com.spring.boot.step.ratelimter;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author snowalker
+ * @version 1.0
+ * @date 2018/10/27 1:25
+ * @className RateLimiter
+ * @desc 限流注解
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RateLimiter {
+
+    /**
+     * 限流key
+     * @return
+     */
+    String key() default "rate:limiter";
+    /**
+     * 单位时间限制通过请求数
+     * @return
+     */
+    long limit() default 10;
+
+    /**
+     * 过期时间，单位秒
+     * @return
+     */
+    long expire() default 1;
+}
